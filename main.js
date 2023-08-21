b58e26191ce5835dd6ca8b1567cb5fe286498efd
Yelous25 <105781548+yelous25@users.noreply.github.com>
2023-08-21 00:11:19 +0000

    feat: recipe-page

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 import './styles/index.css'
+import 'https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js'
 
+let list = document.querySelectorAll('.main .instruction-container ol li')
+console.log(list)
+
+list.forEach((li, key) => {
+  li.querySelector('.olCounter').innerHTML = `<span>${key + 1}</span>`
+})
