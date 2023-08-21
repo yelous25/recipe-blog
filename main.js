d0afca7f9b60636142d7af5ad40f450472364c31
Yelous25 <105781548+yelous25@users.noreply.github.com>
2023-08-21 12:56:19 +0000

    refactor: modify main.js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 import './styles/index.css'
 import 'https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js'
 
-let list = document.querySelectorAll('.main .instruction-container ol li')
-console.log(list)
-
-list.forEach((li, key) => {
+document.querySelectorAll('.main .instruction-container ol li').forEach((li, key) => {
   li.querySelector('.olCounter').innerHTML = `<span>${key + 1}</span>`
 })
